efb1f7c0c132935420edfd53f20528bc7ef184a7
c-wide <53365073+c-wide@users.noreply.github.com>
2024-07-15 16:53:56 -0500

    event file parsing

--- src/utils/event.ts
+++ src/utils/event.ts
@@ -1,19 +1,28 @@
 import type { ClientEvents } from "discord.js";
 import { readdir } from "node:fs/promises";
 import path from "node:path";
+import { z } from "zod";
 
 export type EventHandler<T extends keyof ClientEvents> = (
 	...args: ClientEvents[T]
 ) => void | Promise<void>;
+
+export const eventSchema = z.object({
+	handler: z.function(),
+	name: z.string(),
+	once: z.boolean().optional(),
+});
 
 export async function* eventFiles() {
 	const files = await readdir(path.join(__dirname, "../features"), {
 		recursive: true,
 	});
 
-	const commandFiles = files.filter((file) => file.endsWith(".event.ts"));
+	const eventFiles = files.filter((file) => file.endsWith(".event.ts"));
 
-	for (const file of commandFiles) {
-		yield await import(path.join(__dirname, "../features", file));
+	for (const file of eventFiles) {
+		const event = await import(path.join(__dirname, "../features", file));
+		eventSchema.parse(event);
+		yield event;
 	}
 }
