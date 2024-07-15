656e228cec653369f8e04a1ae3078ec1c0861bee
c-wide <53365073+c-wide@users.noreply.github.com>
2024-07-15 16:52:37 -0500

    initial schema setup

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,7 +1,11 @@
-import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
-export const users = pgTable("users", {
-	id: serial("id").primaryKey(),
-	fullName: text("full_name"),
-	phone: varchar("phone", { length: 256 }),
+export const servers = pgTable("servers", {
+	id: uuid("id").defaultRandom().primaryKey(),
+	serverId: text("server_id").notNull(),
+	name: text("name").notNull(),
+	joinedAt: timestamp("joined_at", { withTimezone: true })
+		.defaultNow()
+		.notNull(),
+	leftAt: timestamp("left_at", { withTimezone: true }),
 });
