022b5fde630af4e12d6defabe165368d7309b0d4
c-wide <53365073+c-wide@users.noreply.github.com>
2024-07-15 23:12:25 -0500

    server settings table

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,4 +1,11 @@
-import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import {
+	pgEnum,
+	pgTable,
+	text,
+	timestamp,
+	unique,
+	uuid,
+} from "drizzle-orm/pg-core";
 
 export const servers = pgTable("servers", {
 	id: uuid("id").defaultRandom().primaryKey(),
@@ -9,3 +16,33 @@ export const servers = pgTable("servers", {
 		.notNull(),
 	leftAt: timestamp("left_at", { withTimezone: true }),
 });
+
+export const dataTypeEnum = pgEnum("data_type", [
+	"string",
+	"number",
+	"boolean",
+	"json",
+]);
+
+export const serverSettings = pgTable(
+	"server_settings",
+	{
+		id: uuid("id").defaultRandom().primaryKey(),
+		serverId: uuid("server_id")
+			.notNull()
+			.references(() => servers.id),
+		key: text("key").notNull(),
+		value: text("value").notNull(),
+		dataType: dataTypeEnum("data_type").notNull(),
+		createdAt: timestamp("created_at", { withTimezone: true })
+			.defaultNow()
+			.notNull(),
+		updatedAt: timestamp("updated_at", { withTimezone: true })
+			.defaultNow()
+			.notNull(),
+		deletedAt: timestamp("deleted_at", { withTimezone: true }),
+	},
+	(t) => ({
+		unq: unique().on(t.serverId, t.key),
+	}),
+);
