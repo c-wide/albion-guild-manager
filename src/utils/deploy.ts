aa7f53fa1cbee90b2b829a202e429961ca01f821
c-wide <53365073+c-wide@users.noreply.github.com>
2024-07-19 16:32:34 -0500

    finish deploy script

--- src/utils/deploy.ts
+++ src/utils/deploy.ts
@@ -1,17 +1,40 @@
 import { REST, Routes } from "discord.js";
 import { commandFiles } from "~/utils/command";
 import { env } from "~/utils/env";
+import { logger } from "~/utils/logger";
+import { getErrorMessage } from "~/utils/misc";
+
+logger.info("Beginning command deployment");
 
 const commands: string[] = [];
 
-for await (const command of commandFiles()) {
-	commands.push(command.builder.toJSON());
+// Parse command files and add builder json to commands array.
+// The generator will handle validating the schema and displaying errors.
+try {
+	for await (const file of commandFiles()) {
+		commands.push(file.builder.toJSON());
+	}
+} catch (_) {
+	logger.fatal("Command deployment failed due to invalid command files");
+	process.exit(1);
 }
 
 const rest = new REST().setToken(env.DISCORD_TOKEN);
 
+// If guild id env var is set, deploy commands to guild, otherwise deploy globally
 const route = env.DISCORD_GUILD_ID
 	? Routes.applicationGuildCommands(env.DISCORD_CLIENT_ID, env.DISCORD_GUILD_ID)
 	: Routes.applicationCommands(env.DISCORD_CLIENT_ID);
 
-await rest.put(route, { body: commands });
+try {
+	await rest.put(route, { body: commands });
+
+	logger.info(
+		`Successfully deployed all commands ${
+			env.DISCORD_GUILD_ID ? `to guild ${env.DISCORD_GUILD_ID}` : "globally"
+		}`,
+	);
+} catch (e) {
+	logger.fatal(`Command deployment failed: ${getErrorMessage(e)}`);
+	process.exit(1);
+}
