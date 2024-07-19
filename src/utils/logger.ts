0d6295ebb6e24c7951c89a0cea712d5a03dc7471
c-wide <53365073+c-wide@users.noreply.github.com>
2024-07-19 13:43:03 -0500

    logger use config

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,10 +1,12 @@
 import pino from "pino";
 import { env } from "~/utils/env";
+import { config } from "~/utils/config";
 
 const targets: pino.TransportTargetOptions[] = [];
 
 if (env.AXIOM_TOKEN) {
 	targets.push({
+		level: "info",
 		target: "@axiomhq/pino",
 		options: {
 			token: env.AXIOM_TOKEN,
@@ -25,7 +27,7 @@ if (env.PINO_PRETTY === "true") {
 
 export const logger = pino(
 	{
-		level: "info",
+		level: config.logLevel,
 		timestamp: () => `,"timestamp":"${new Date(Date.now()).toISOString()}"`,
 	},
 	targets.length > 0 && pino.transport({ targets: targets }),
