7d8b986f452b121c0f3d28ab83298482e1722c45
c-wide <53365073+c-wide@users.noreply.github.com>
2024-07-22 00:28:38 -0500

    register events and once required

--- src/utils/event.ts
+++ src/utils/event.ts
@@ -1,17 +1,25 @@
-import type { ClientEvents } from "discord.js";
+import type { Client, ClientEvents } from "discord.js";
 import { readdir } from "node:fs/promises";
 import path from "node:path";
 import { z } from "zod";
 
-export type EventHandler<T extends keyof ClientEvents> = (
+export type EventName = keyof ClientEvents;
+
+export type EventHandler<T extends EventName> = (
 	...args: ClientEvents[T]
 ) => void | Promise<void>;
 
 export const eventSchema = z.object({
 	handler: z.function(),
 	name: z.string(),
-	once: z.boolean().optional(),
+	once: z.boolean(),
 });
+
+export async function registerEvents(client: Client) {
+	for await (const file of eventFiles()) {
+		client[file.once ? "once" : "on"](file.name, file.handler);
+	}
+}
 
 export async function* eventFiles() {
 	const files = await readdir(path.join(__dirname, "../features"), {
