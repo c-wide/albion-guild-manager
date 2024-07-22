6b49225faa9a04a0b0e4ae57e55dac139b6f8503
c-wide <53365073+c-wide@users.noreply.github.com>
2024-07-22 00:30:41 -0500

    utc cooldown and i18n

--- src/features/utc.command.ts
+++ src/features/utc.command.ts
@@ -1,12 +1,20 @@
 import { SlashCommandBuilder } from "discord.js";
 import type { CommandHandler } from "~/utils/command";
+import i18n from "~/utils/i18n";
+
+export const cooldown = 5;
 
 export const builder = new SlashCommandBuilder()
-	.setName("utc")
-	.setDescription("Get the current UTC time");
+	.setName(i18n.t("cmd-utc-name"))
+	.setDescription(i18n.t("cmd-utc-desc"));
 
 export const handler: CommandHandler = async (i) => {
-	await i.reply(
-		`The current UTC time is ${new Date().toUTCString().split(" ")[4]}`,
-	);
+	await i.reply({
+		content: i18n.t("cmd-utc-response", {
+			utcTime: new Date().toUTCString().split(" ")[4],
+			lng: i.locale,
+			fallbackLng: "en",
+		}),
+		ephemeral: true,
+	});
 };
--- src/locales/en/common.ts
+++ src/locales/en/common.ts
@@ -1,3 +1,5 @@
 export const common = {
-	botReady: "Ready! Logged in as {{discordTag}}",
+	"cmd-utc-name": "utc",
+	"cmd-utc-desc": "Get the current UTC time",
+	"cmd-utc-response": ":clock3: **{{utcTime}}**",
 } as const;
