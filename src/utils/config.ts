74493feb57a2a9b4d1e9dbaeffeea62fb52e039c
c-wide <53365073+c-wide@users.noreply.github.com>
2024-07-28 13:29:04 -0500

    add server regions to config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -3,4 +3,7 @@ import type { Level } from "pino";
 export const config = {
 	logLevel: "info" satisfies Level,
 	guildChangeKeys: ["name", "nameAcronym", "iconURL", "bannerURL"],
+	albionServerRegions: ["Americas", "Asia", "Europe"],
 } as const;
+
+export type AlbionServerRegion = (typeof config.albionServerRegions)[number];
